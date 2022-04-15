db8ce40e7f738c43189f7e21c85834fee66d4364
Stanislaw Jakubowski <sjakubowski3554@gmail.com>
2022-04-15 17:18:45 +0200

    Deleted api, created products.ts with shoes data

--- src/components/shop/Shop.tsx
+++ src/components/shop/Shop.tsx
@@ -1,57 +1,12 @@
-import { useEffect, useState } from 'react'
-
-interface productInterface {
-	id: number
-	title: string
-	price: number
-	description: string
-	category: string
-	image: string
-	rating: {
-		rate: number
-		count: number
-	}
-}
-
-const url = 'https://fakestoreapi.com/products'
+import { productInterface, products } from '../../products'
+import { ShopItem } from '../shop_item/ShopItem'
 
 export const Shop = () => {
-	const [products, setProducts] = useState<productInterface[]>()
-	const [isError, setIsError] = useState(false)
-	const [isLoading, setIsLoading] = useState(false)
-
-	const fetchProducts = async (url: string) => {
-		try {
-			setIsError(false)
-			setIsLoading(true)
-			const res = await fetch(url)
-			const data = await res.json()
-			setProducts(data)
-			setIsLoading(false)
-		} catch (err) {
-			setIsError(true)
-			setIsLoading(false)
-			console.log(err)
-		}
-	}
-
-	useEffect(() => {
-		fetchProducts(url).catch(err => {
-			setIsError(true)
-			console.log(err)
-		})
-	}, [])
-
-	if (isLoading) {
-		return <h2>Loading...</h2>
-	}
-
-	if (isError) {
-		return <h2>Something went wrong!</h2>
-	}
-
-	if (products) {
-		return <div>{products.map((product: productInterface) => product.id)}</div>
-	}
-	return null
+	return (
+		<div className='columns-1'>
+			{products.map((product: productInterface) => (
+				<ShopItem {...product} />
+			))}
+		</div>
+	)
 }
--- src/components/shop_item/ShopItem.tsx
+++ src/components/shop_item/ShopItem.tsx
@@ -1,3 +1,12 @@
-export const ShopItem = () => {
-	return <div></div>
+import { productInterface } from '../../products'
+
+export const ShopItem = ({ id, category, description, images, price }: productInterface) => {
+	return (
+		<div className='relative m-5 last:hover:top-0'>
+			<img src={images[0]} alt='' className='h-100' />
+			<div className='absolute bottom-0 left-0 right-0 top-3/4 bg-primary/90 '>
+				<h3 className='text-center '>{description}</h3>
+			</div>
+		</div>
+	)
 }
